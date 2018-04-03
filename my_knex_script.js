92bc7c6cf759c0a18e0994cabe0a24a09c1b402a
Sam Schantz <samvschantz@gmail.com>
2018-04-03 21:50:48 +0000

    Knex impement

---
+++ my_knex_script.js
@@ -0,0 +1,26 @@
+const settings = require("./settings");
+const name = process.argv[2]
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+knex.select('*').from('famous_people').where(`first_name`, `like` ,`%${name}%`)
+  .asCallback((err, res) =>{
+    if (err) throw err
+    console.log(`Found ${res.length} person(s) by the name '${name}':`)
+    for (person of res){
+      let first_name = person.first_name
+      let last_name = person.last_name
+      let birthdate = person.birthdate.toISOString().slice(0,10)
+      console.log(`-: ${first_name} ${last_name}, born '${birthdate}'`)
+    }
+  });
